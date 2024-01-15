0ed7d5334786249afd6efdef40357000a8138ec2
LeRoi Vladimir <banyamkimbi@vrfound.org>
2024-01-15 13:10:03 +0100

    creation of authentication middleware

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -31,7 +31,7 @@ export const register = async (req, res) => {
         })
     } catch (error) {
         res.status(500).json({
-            error: err.message
+            error: error.message
         })
     }
 }
@@ -50,7 +50,15 @@ export const login = async ( req, res ) => {
         if (!isMatch) return res.status(400).json({msg: "Invalid credentials."})
 
         const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+        delete user.password
+        res.status(200).json({
+            token,
+            user
+        })
     }catch(error) {
+        res.status(500).json({
+            error: error.message
+        })
 
     }
 }
