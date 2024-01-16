062f606fa0c546f007263a0b7b3fb9c363d99508
LeRoi Vladimir <banyamkimbi@vrfound.org>
2024-01-16 21:06:46 +0100

    setting up redux config

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import Home from "./pages/homePage/Home";
+import Login from "./pages/loginPage/Login";
+import Profile from "./pages/profilePage/Profile";
+import { useState } from "react";
+
 
 function App() {
+
+  // const mode = useState
+
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
